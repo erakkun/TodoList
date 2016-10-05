f49cb2b77dcf6a4a3fa78c51f0294082833901ee
Joona Melartin <erakko92@gmail.com>
2016-10-05 11:00:57 +0300

    Update, basics seem to work.

--- app/assets/javascripts/edit.js
+++ app/assets/javascripts/edit.js
@@ -1,12 +1,33 @@
 /**
  * Created by joona on 4.10.2016.
  */
+
 $(document).ready(function() {
     $(".task-edit").hide();
 
     $("button.edit-button").click(function(){
-        var field = $(this).parent().parent().parent();
+        var field = $(this).closest(".task");
         field.find(".task-list").toggle();
         field.find(".task-edit").toggle();
-    })
+    });
+
+    $("button.save-button").click(function(){
+        var field = $(this).closest(".task").find(".task-edit");
+        var title = field.find("input[name='title']");
+        var deadline = field.find("input[name='deadline']");
+        var task_type_id = field.find("select[name='task_type_id']");
+
+       $.ajax({
+           type: "POST",
+           url: "/tasks",
+           data: {
+               task: {
+                   title: title,
+                   task_type_id: task_type_id,
+                   deadline: deadline
+
+               }
+           }
+       })
+    });
 });
